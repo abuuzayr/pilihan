c06913f6dc1808137b559d70d35cfec050c2a848
Abu Uzayr <abu.uzayr@builtforfifty.com>
2019-06-09 20:04:35 +0800

    Add index page components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Helmet from "react-helmet"
+import PilihanApp from "../components/PilihanApp"
 
-export default () => <div id="app"></div>
+export default () => (
+    <div>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Pilihan - Choose!</title>
+        </Helmet>
+        <PilihanApp />
+    </div>
+)
