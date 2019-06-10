85b278ee6436a16e5129c76abfacf3a72d9237b2
Abu Uzayr <abu.uzayr@builtforfifty.com>
2019-06-10 19:23:22 +0800

    Add styling for Modal component

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import Modal from 'react-modal';
+import styled from 'styled-components';
+
+const ModalTitle = styled.h3`
+    margin: 0 0 var(--m-size) 0;
+`
+
+const ModalBody = styled.div`
+    font-size: 2rem;
+    font-weight: 300;
+    margin: 0 0 var(--l-size) 0;
+    word-break: break-all;
+`
 
 const OptionModal = (props) => (
     <Modal
         isOpen={!!props.modalText}
-        onRequestClose={props.closeModal}>
-        <h3>{props.modalText}</h3>
-        <button onClick={props.closeModal}>OK</button>
+        onRequestClose={props.closeModal}
+        contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal">
+        <ModalBody>
+            <ModalTitle>Selected Option</ModalTitle>
+            {props.modalText && <p className="modal__body">{props.modalText}</p>}
+            <button onClick={props.closeModal}>OK</button>
+        </ModalBody>
     </Modal>
 );
 
