9a495c064e129355d1cc99bffc56fa0b189ee31b
Abu Uzayr <abu.uzayr@builtforfifty.com>
2019-06-10 19:23:12 +0800

    Add styling for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    max-width: 60rem;
+    margin: 0 auto;
+    padding: 0 var(--m-size);
+`
+
+const HeaderDiv = styled.div`
+    background: var(--off-black);
+    color: white;
+    margin-bottom: var(--m-size);
+    padding: var(--m-size) 0;
+
+    @media (min-width: 45rem) {
+        margin-bottom: var(--xl-size);
+    }
+`
+
+const Title = styled.div`
+    font-size: var(--l-size);
+    margin: 0;
+`
+
+const Subtitle = styled.div`
+    color: var(--off-white);
+    font-size: var(--m-size);
+    font-weight: 500;
+    margin: 0;
+`
 
 const Header = (props) => (
-    <div>
-        <h1>{props.title}</h1>
-        <h2>{props.subtitle}</h2>
-    </div>
+    <HeaderDiv>
+        <Container>
+            <Title>{props.title}</Title>
+            <Subtitle>{props.subtitle}</Subtitle>
+        </Container>
+    </HeaderDiv>
 );
 
 Header.defaultProps = {
-    title: 'Hey ho let\'s go',
-    subtitle: 'Subtitle!'
+    title: 'Pilihan',
+    subtitle: 'Choice randomizer app'
 }
 
 export default Header;
