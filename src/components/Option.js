c95b91727099b904d1fe534355e495b03b12e4e0
Abu Uzayr <abu.uzayr@builtforfifty.com>
2019-06-10 19:23:17 +0800

    Add styling for Option component

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Text = styled.p`
+    color: white;
+    font-weight: 500;
+    font-size: 2rem;
+    margin: 0;
+    word-break: break-all;
+`
+
+const Li = styled.li`
+    border-bottom: 1px solid var(--light-blue);
+    display: flex;
+    justify-content: space-between;
+    padding: var(--l-size) var(--m-size);
+`
+
+const Button = styled.button`
+    background: var(--purple);
+    border: none;
+    border-bottom: .3rem solid darken(var(--purple), 10%);
+    color: white;
+    font-weight: 500;
+    padding: var(--s-size);
+`
 
 const Option = (props) => (
-    <li>{props.option}
-        <button onClick={(e) => {
+    <Li>
+        <Text>
+            {props.option}
+        </Text>
+        <Button onClick={(e) => {
             props.handleRemove(props.option)
         }}>
             Remove
-        </button>
-    </li>
+        </Button>
+    </Li>
 );
 
 export default Option;
