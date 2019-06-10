ae5d16560320c3ba3eea476f8148624ab37c589c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2019-06-10 12:49:42 +0800

    Add style to action component

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+    background: var(--purple);
+    border: none;
+    border-bottom: .3rem solid darken(var(--purple), 10%);
+    color: white;
+    font-weight: 500;
+    padding: var(--s-size);
+
+    ${props => props.isLink && css`
+        background: none;
+        border: none;
+        color: $off-white;
+        padding: 0;
+    `}
+`
 
 const Action = (props) => (
     <div>
-        <button
+        <Button
             onClick={props.handlePick}
             disabled={!props.hasOptions}
         >
             What should I do?
-                </button>
+        </Button>
     </div>
 );
 
